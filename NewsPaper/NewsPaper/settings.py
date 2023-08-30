efc2526c80f656c4c72eb612d2855eb05d90de8b
Aleksej Savica <sova11@mail.ru>
2023-08-30 16:09:22 +0300

    добавил python-dotenv

--- NewsPaper/NewsPaper/settings.py
+++ NewsPaper/NewsPaper/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -210,13 +213,12 @@ EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
 EMAIL_USE_SSL = True
 
-EMAIL_HOST_USER = 'skillfactory.course'
-EMAIL_HOST_PASSWORD = 'ivyohjnbbi'
 
-# DEFAULT_FROM_EMAIL = 'skillfactory.course@yandex.ru'
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER + '@yandex.ru'
-SERVER_EMAIL = EMAIL_HOST_USER
-EMAIL_ADMIN = EMAIL_HOST_USER
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
+
+
 
     
 # ===================== django_apscheduler ==================
